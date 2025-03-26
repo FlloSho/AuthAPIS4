0817bfdbd5d96733be94272d336670332e120f64
FloSho <florian.lepronguillout@gmail.com>
2025-03-26 11:01:01 +0100

    Requête CORS

--- api/authApi.php
+++ api/authApi.php
@@ -3,10 +3,17 @@ global $pdo;
 require_once "../config/db.php";
 require_once "../controllers/authController.php";
 
+// Définir les en-têtes CORS au début
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Gérer les requêtes préliminaires OPTIONS pour CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 $authController = new AuthController($pdo);
 $response = json_decode(file_get_contents("php://input"), true);
